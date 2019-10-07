42df1871855784d16e21944b2f95a8fe52e40175
Igor Kling <klingiv@altarix.ru>
2019-10-07 13:07:45 +0400

    custom writable stream

--- child.js
+++ child.js
@@ -3,7 +3,7 @@ let counter = 0
 const handler = setInterval(() => {
   process.stdout.write(`${counter++}\n`)
 
-  if (counter > 100) {
+  if (counter > 10000) {
     clearInterval(handler)
   }
 }, 0)
--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+const { Writable } = require('stream')
 const { spawn, fork } = require('child_process')
 const path = require('path')
 
+const writer = new (class extends Writable {
+  constructor() {
+    super()
+  }
+  _write(chunk, encoding, next) {
+    // console.log(chunk.toString())
+    next()
+  }
+})()
+
 ;(async () => {
   const child = spawn(process.execPath, [path.resolve(__dirname, './child.js')], {
-    stdio: ['pipe', 'pipe', 'pipe']
+    stdio: ['ignore', 'pipe', 'pipe']
   })
 
   // child.stdout.on('data', data => {
@@ -14,5 +25,5 @@ const path = require('path')
     console.log('DONE')
   })
 
-  child.stdout.pipe(process.stdout)
+  child.stdout.pipe(writer /*process.stdout*/)
 })()
